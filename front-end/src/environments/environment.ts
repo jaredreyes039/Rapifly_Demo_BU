1071c4597eb3f210dbb31922642d164f79832541
jaredreyes039 <jaredreyes039@gmail.com>
2023-02-08 21:08:00 -0700

    Fixed env vars following dev servr

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://lionfish-app-czku6.ondigitalocean.app/',
   socketUrl: 'http://localhost:4000',
 };
 
